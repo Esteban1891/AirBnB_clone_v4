5ffd5652b14cb687142405047b5d4689c29948b4
EduardoVega04 <eduarvega04@hotmail.com>
2020-09-29 22:42:11 -0500

    Script that starts a Flask web application

--- web_dynamic/0-hbnb.py
+++ web_dynamic/0-hbnb.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-""" Script that  Starts a Flash Web Application """
-
+""" Starts a Flash Web Application """
 from models import storage
 from models.state import State
 from models.city import City
@@ -8,8 +7,9 @@ from models.amenity import Amenity
 from models.place import Place
 from os import environ
 from flask import Flask, render_template
-import uuid
 app = Flask(__name__)
+# app.jinja_env.trim_blocks = True
+# app.jinja_env.lstrip_blocks = True
 
 
 @app.teardown_appcontext
@@ -34,11 +34,10 @@ def hbnb():
     places = storage.all(Place).values()
     places = sorted(places, key=lambda k: k.name)
 
-    return render_template('0-hbnb.html',
+    return render_template('100-hbnb.html',
                            states=st_ct,
                            amenities=amenities,
-                           places=places,
-                           cache_id=uuid.uuid4())
+                           places=places)
 
 
 if __name__ == "__main__":
