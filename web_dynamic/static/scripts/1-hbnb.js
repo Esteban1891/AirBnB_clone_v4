f62e0f122b9c65eb2328dce9f4cc6c49b4d75767
EduardoVega04 <eduarvega04@hotmail.com>
2020-10-01 23:46:22 -0500

    Masters in css

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,14 +1,24 @@
-$(document).ready(function () {
-  const myAmenities = {};
-  $('div.amenities .popover ul li').click(function () {
-    const amenityName = $(this).getAttribute('data-name');
-    const amenityID = $(this).getAttribute('data-id');
+$(document).ready(function() { 
+  let myAmenities = [];
+  $('div .amenities .popover ul li INPUT').click(function() {     
+    let amenityName = $(this).attr('data-name'); 
+    let amenityID = $(this).attr('data-id');    
+    
+    $('div .amenities h4').css({
+      width: '98%',
+      height: '98%',
+      'white-space': 'nowrap',
+      overflow: 'hidden',
+      'text-overflow': 'ellipsis',
+      'padding-bottom': '16px'
+    });
 
-    if ($(this).checked === false) {
-      delete myAmenities[amenityID];
-    } else {
-      myAmenities[amenityID] = amenityName;
-    }
-    $('div.amenities h4').append(myAmenities[amenityID]);
+    if ($(this).is(':checked')) {      
+      myAmenities.push(amenityName);
+    } else {      
+      let idx = myAmenities.indexOf(amenityName);
+      myAmenities.splice(idx, 1);
+    }    
+    $('div.amenities h4').text(myAmenities.join(', '));          
   });
 });
