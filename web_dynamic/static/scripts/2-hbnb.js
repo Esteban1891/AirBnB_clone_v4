c9cf3c3f73226171b635bb59679e3c6a7de0a8f0
EduardoVega04 <eduarvega04@hotmail.com>
2020-10-03 05:07:24 -0500

    Fixed task 3

--- web_dynamic/static/scripts/2-hbnb.js
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -1,9 +1,7 @@
-$(document).ready(function () {  
-  $.get('http://localhost:5001/api/v1/status', function (data, status) {
-    if (status === 'OK') {
-      $('div#api_status').addClass('available');
-    } else {
-      $('div#api_status').removeClass('available');
-    }
+$(document).ready(function(){   
+  $.get('http://localhost:5001/api/v1/status', function(data, status){
+    if (data.status === "OK") {      
+      $('div#api_status').addClass('available');     
+    } 
   });
 });
